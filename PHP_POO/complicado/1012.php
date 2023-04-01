7c51be291eeed844a49fbf7b7efcf1074e973661
AdalbertoM <37272667+AdalbertoM@users.noreply.github.com>
2023-04-01 15:47:13 -0300

    Add files via upload

---
+++ PHP_POO/complicado/1012.php
@@ -0,0 +1,14 @@
+<?php
+$valores = explode(" ", fgets(STDIN));
+$areaTriRetangulo = number_format((($valores[0] * $valores[2]) / 2), 3, '.', '');
+$areaCirculo = number_format(3.14159 * pow($valores[2], 2), 3, '.', '');
+$areaTrapezio = number_format(((($valores[0]+$valores[1]) * $valores[2]) / 2), 3, '.', '');
+$areaQuadrado = number_format(pow($valores[1], 2), 3, '.', '');
+$areaRetangulo = number_format(($valores[0] * $valores[1]), 3, '.', '');
+
+echo "TRIANGULO: $areaTriRetangulo\n";
+echo "CIRCULO: $areaCirculo\n";
+echo "TRAPEZIO: $areaTrapezio\n";
+echo "QUADRADO: $areaQuadrado\n";
+echo "RETANGULO: $areaRetangulo\n";
+?>
