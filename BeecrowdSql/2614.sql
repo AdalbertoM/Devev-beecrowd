e9a801a23a2accb0ae077df6fa310c1cac80ef4b
AdalbertoM <37272667+AdalbertoM@users.noreply.github.com>
2023-04-10 20:30:46 -0300

    Add files via upload

---
+++ BeecrowdSql/2614.sql
@@ -0,0 +1,3 @@
+select customers.name,rentals.rentals_date from customers
+join rentals on customers.id = rentals.id_customers
+where extract(month from rentals.rentals_date) = 9;
---
+++ BeecrowdSql/2995.sql
@@ -0,0 +1,2 @@
+SELECT temperature, COUNT(mark) FROM records
+GROUP BY temperature, mark ORDER BY mark
